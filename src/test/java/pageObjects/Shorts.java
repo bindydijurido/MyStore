9de8566ac367de734a36fdbfdd0a4a62993da5b4
Bindy <filip.skwierczynski@gmail.com>
2018-01-03 16:12:31 +0100

    refactor

--- src/test/java/pageObjects/Shorts.java
+++ src/test/java/pageObjects/Shorts.java
@@ -6,14 +6,11 @@ import utility.Root;
 
 public class Shorts extends Root {
 
-    private static String element;
-
     public static void openMainMenu() {
         driver.get("http://automationpractice.com/index.php");
     }
 
     protected static void log(String element) {
-        Shorts.element = element;
         System.out.println(element);
     }
 
